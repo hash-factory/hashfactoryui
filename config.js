2cc5a1a18c347c8cc4534cf6af8329adbddd998a
Interchained <president@worldvaporexpo.com>
2018-09-11 18:05:39 -0400

    update css variations

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 var api = "https://etnx.hashfactory.online:8119";
 
 var email = "support@electronero.org";
-var telegram = "https://t.me/etnxp";
+var telegram = "https://t.me/etnxpool";
 var discord = "https://discord.gg/YMnPXEZ";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "https://explorer.electroneropulse.org/block/{id}";
-var transactionExplorer = "https://explorer.electroneropulse.org/tx/{id}";
+var blockchainExplorer = "https://blockexplorer.electronero.org/block/{id}";
+var transactionExplorer = "https://blockexplorer.electronero.org/tx/{id}";
 
-var themeCss = "themes/default.css";
+var themeCss = "themes/dark.css";
 var defaultLang = 'en';
