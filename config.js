2c5a87228861e1feb115517e728743bbc5ba93d1
Mark Allen Evans <president@worldvaporexpo.com>
2018-09-11 22:03:11 +0000

    initials

---
+++ config.js
@@ -0,0 +1,13 @@
+var api = "https://etnx.hashfactory.online:8119";
+
+var email = "support@electronero.org";
+var telegram = "https://t.me/etnxp";
+var discord = "https://discord.gg/YMnPXEZ";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.electroneropulse.org/block/{id}";
+var transactionExplorer = "https://explorer.electroneropulse.org/tx/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
