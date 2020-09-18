4a9ee2fc9c3e61c0681d54ff96d2c991d1578c60
Mark Allen Evans <president@worldvaporexpo.com>
2020-09-18 03:45:17 +0000

    :hash

---
+++ configure.js
@@ -0,0 +1,13 @@
+var api = "https://pool.crystaleum.org:8116";
+
+var email = "support@crystaleum.org";
+var telegram = "https://t.me/crystaleum";
+var discord = "https://discord.gg/YMnPXEZ";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://oracle.crystaleum.org/block/{id}";
+var transactionExplorer = "https://ora.crystaleum.org/tx/{id}";
+
+var themeCss = "themes/dark.css";
+var defaultLang = 'en';
